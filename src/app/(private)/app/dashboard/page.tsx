ece42753938ad66a7f8e5efa76ee44b10e017e1c
gumrahsindar <gsindar@gmail.com>
2024-10-15 13:08:24 +0300

    feat: fetch pet data with server component and show it in pet-list

--- src/app/(private)/app/dashboard/page.tsx
+++ src/app/(private)/app/dashboard/page.tsx
@@ -1,12 +1,43 @@
 import Branding from '@/components/branding'
+import ContentBlock from '@/components/content-block'
+import PetDetails from '@/components/pet-details'
+import PetList from '@/components/pet-list'
+import SearchForm from '@/components/search-form'
 import Stats from '@/components/stats'
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  )
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch pets')
+  }
+  const data = await response.json()
+
   return (
     <main>
       <div className='flex items-center justify-between text-white py-8'>
         <Branding />
         <Stats />
+      </div>
+
+      <div className='grid md:grid-cols-3 grid-rows-[45px_300px_500px] md:grid-rows-[45px_1fr] gap-4 md:h-[600px]'>
+        <div className='md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1'>
+          <SearchForm />
+        </div>
+
+        <div className='md:row-start-2 md:row-span-full md:col-start-1 col-span-1'>
+          <ContentBlock>
+            <PetList pets={data} />
+          </ContentBlock>
+        </div>
+
+        <div className='md:row-start-1 md:row-span-full md:col-start-2 col-span-full'>
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   )
---
+++ src/components/pet-list.tsx
@@ -0,0 +1,27 @@
+import { type Pet } from '@/lib/types'
+import Image from 'next/image'
+
+type PetListProps = {
+  pets: Pet[]
+}
+
+export default function PetList({ pets }: PetListProps) {
+  return (
+    <ul className='bg-white border-b border-black/[0.08]'>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className='flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:[bg-[#eff1f2] transition-colors]'>
+            <Image
+              src={pet.imageUrl}
+              alt={pet.name}
+              width={45}
+              height={45}
+              className='size-[45px] rounded-full object-cover'
+            />
+            <p className='font-semibold'>{pet.name}</p>
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
