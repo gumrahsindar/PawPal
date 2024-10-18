532b1e6dd39e765f8bf71f808fcf672528ac8c9e
gumrahsindar <gsindar@gmail.com>
2024-10-18 15:26:09 +0300

    feat: create pet context and list fetching data up to the layout

--- src/app/(private)/app/layout.tsx
+++ src/app/(private)/app/layout.tsx
@@ -1,14 +1,28 @@
 import AppFooter from '@/components/app-footer'
 import AppHeader from '@/components/app-header'
 import BackgroundPattern from '@/components/background-pattern'
+import PetContextProvider from '@/contexts/pet-context-provider'
+import { Pet } from '@/lib/types'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  )
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch pets')
+  }
+  const data: Pet[] = await response.json()
   return (
     <>
       <BackgroundPattern />
       <div className='min-h-screen flex flex-col max-w-[1050px] mx-auto px-4'>
         <AppHeader />
-        {children}
+        <PetContextProvider data={data}>{children}</PetContextProvider>
         <AppFooter />
       </div>
     </>
