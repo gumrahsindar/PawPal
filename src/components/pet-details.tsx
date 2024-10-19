ec66b1e3bff0dff148fcd97a98b0cdbe79b8cc6a
gumrahsindar <gsindar@gmail.com>
2024-10-19 13:39:42 +0300

    feat: add pet-details and functionality

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -1,3 +1,73 @@
+'use client'
+
+import usePetContext from '@/lib/hooks/usePetContext'
+import { Pet } from '@/lib/types'
+import Image from 'next/image'
+
 export default function PetDetails() {
-  return <section className='h-full w-full'>Pet details</section>
+  const { selectedPet } = usePetContext()
+  return (
+    <section className='flex flex-col h-full w-full'>
+      {!selectedPet && <EmptyView />}
+
+      {selectedPet && (
+        <>
+          <TopBar pet={selectedPet} />
+          <OtherInfo pet={selectedPet} />
+          <Notes pet={selectedPet} />
+        </>
+      )}
+    </section>
+  )
+}
+
+type Props = {
+  pet: Pet
+}
+
+function EmptyView() {
+  return (
+    <p className='text-2xl font-medium h-full flex justify-center items-center'>
+      No pet selected
+    </p>
+  )
+}
+
+function TopBar({ pet }: Props) {
+  return (
+    <div className='flex items-center bg-white px-8 py-5 border-b border-light'>
+      <Image
+        src={pet?.imageUrl}
+        alt='Selected pet image'
+        height={75}
+        width={75}
+        className='h-[75px] w-[75px] rounded-full object-cover'
+      />
+      <h2 className='text-3xl font-semibold leading-7 ml-5'>{pet?.name}</h2>
+    </div>
+  )
+}
+
+function OtherInfo({ pet }: Props) {
+  return (
+    <div className='flex justify-around py-10 px-5 text-center'>
+      <div>
+        <h3 className='font-medium uppercase text-zinc-700'>Owner name</h3>
+        <p className='mt-1 text-lg text-zinc-800'>{pet?.ownerName}</p>
+      </div>
+
+      <div>
+        <h3 className='font-medium uppercase text-zinc-700'>Age</h3>
+        <p className='mt-1 text-lg text-zinc-800'>{pet?.age}</p>
+      </div>
+    </div>
+  )
+}
+
+function Notes({ pet }: Props) {
+  return (
+    <div className='flex-1 bg-white px-7 py-5 mb-9 mx-8 border border-light'>
+      {pet?.notes}
+    </div>
+  )
 }
