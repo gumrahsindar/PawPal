04efc22a41bfde37e671fd0ceed5f9c188acb44a
gumrahsindar <gsindar@gmail.com>
2024-10-23 13:35:08 +0300

    feat: add search context for user search

---
+++ src/contexts/search-context-provider.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState, createContext } from 'react'
+
+type SearchContextProviderProps = {
+  children: React.ReactNode
+}
+
+type TSearchContext = {
+  searchQuery: string
+  handleChangeSearchQuery: (newValue: string) => void
+}
+
+export const SearchContext = createContext<TSearchContext | null>(null)
+
+export default function SearchContextProvider({
+  children,
+}: SearchContextProviderProps) {
+  // state
+  const [searchQuery, setSearchQuery] = useState('')
+
+  // derived state
+
+  // handlers
+  const handleChangeSearchQuery = (newValue: string) => {
+    setSearchQuery(newValue)
+  }
+
+  console.log('searchQuery', searchQuery)
+
+  return (
+    <SearchContext.Provider
+      value={{
+        searchQuery,
+        handleChangeSearchQuery,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  )
+}
---
+++ src/lib/hooks/useSearchContext.ts
@@ -0,0 +1,13 @@
+import { SearchContext } from '@/contexts/search-context-provider'
+import { useContext } from 'react'
+
+export default function useSearchContext() {
+  const context = useContext(SearchContext)
+
+  if (!context) {
+    throw new Error(
+      'useSearchContext must be used within a SearchContextProvider'
+    )
+  }
+  return context
+}
