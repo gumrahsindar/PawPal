a7a2c8ffe6bf50efd755e6e29430d7a1ebc2a667
gumrahsindar <gsindar@gmail.com>
2024-11-05 15:01:11 +0300

    feat: replace API fetch with Prisma query to retrieve pets in layout component

--- src/app/(private)/app/layout.tsx
+++ src/app/(private)/app/layout.tsx
@@ -3,28 +3,21 @@ import AppHeader from '@/components/app-header'
 import BackgroundPattern from '@/components/background-pattern'
 import PetContextProvider from '@/contexts/pet-context-provider'
 import SearchContextProvider from '@/contexts/search-context-provider'
-import { Pet } from '@/lib/types'
+import prisma from '@/lib/db'
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const response = await fetch(
-    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
-  )
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch pets')
-  }
-  const data: Pet[] = await response.json()
+  const pets = await prisma.pet.findMany()
   return (
     <>
       <BackgroundPattern />
       <div className='min-h-screen flex flex-col max-w-[1050px] mx-auto px-4'>
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider data={data}>{children}</PetContextProvider>
+          <PetContextProvider data={pets}>{children}</PetContextProvider>
         </SearchContextProvider>
         <AppFooter />
       </div>
