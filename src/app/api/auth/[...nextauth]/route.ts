9501e3d1c2491cfbb794a36f121b83bc417e3317
Gümrah Sindar <gsindar@gmail.com>
2025-06-10 22:20:04 +0300

    refactor: enhance export formatting and add comments for clarity in auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,5 @@
-export { GET, POST } from '@/lib/auth'
+export {
+  /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
+  GET /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/,
+  POST,
+} from "@/lib/auth"
